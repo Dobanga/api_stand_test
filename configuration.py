54154f3d982f37e101568deb0f41e55a4f227fc6
Jesús Jovan Robles Osuna <jesus_jovan@hotmail.com>
2025-03-30 19:07:36 -0700

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://cnt-6b4eec45-46db-482d-927e-3c89534de900.containerhub.tripleten-services.com"
+#copia la URL generada sin la barra inclinada al final
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
